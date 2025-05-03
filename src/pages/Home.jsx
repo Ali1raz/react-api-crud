2627ddb6831b76a489a5ed8ab9127120a69f2caf
Ali Raza <alirazakhalid1129@gmail.com>
2025-05-03 20:53:24 +0500

    fix layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,27 +25,33 @@ export default function Home() {
       <h1 className="title">Latest posts</h1>
       <div className=" max-w-3xl mx-auto mt-10">
         {posts.length > 0 ? (
-          <div className="">
+          <div className="max-sm:max-w-xl">
             {posts.map((post) => (
               <div
                 key={post.id}
                 className={clsx(
                   "border-2 border-black border-dashed p-5 mb-5 rounded-sm shadow-md",
-                  user?.id === post.user.id && "border-blue-500"
+                  user?.id === post.user_id && "border-blue-500"
                 )}
               >
                 <div className="flex justify-between items-center mb-2">
                   <Link to={`/post/${post.id}`}>
-                    <h2 className="text-xl font-bold">{post.title}</h2>
+                    <h2 className="text-xl truncate max-w-[36ch] max-sm:max-w-[20ch] max-sm:text-sm font-bold">
+                      {post.title}
+                    </h2>
                   </Link>
                   <div className="space-x-2">
-                    <small>{post.user.name}</small>
+                    <small className="truncate max-w-6 max-sm:max-w-[4ch]">
+                      {post.user.name}
+                    </small>
                     <small className="text-sm text-gray-500">
                       {new Date(post.created_at).toLocaleDateString()}
                     </small>
                   </div>
                 </div>
-                <p>{post.body}</p>
+                <p className="truncate max-w-[70ch] max-sm:max-w-[40ch]">
+                  {post.body}
+                </p>
               </div>
             ))}
           </div>
